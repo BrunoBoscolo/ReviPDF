e18a699237ef64e03aea86ef82cbf2c64734e344
BrunoBoscolo <bruno.boscolo2304@gmail.com>
2026-03-17 10:00:53 -0300

    a

--- revipdf/src-tauri/src/main.rs
+++ revipdf/src-tauri/src/main.rs
@@ -1,6 +1,9 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// src-tauri/src/main.rs
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    revipdf_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init()) // <-- ADICIONE ESTA LINHA AQUI
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
